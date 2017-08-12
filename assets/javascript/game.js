9bf22d52bcfdbbe7222c79fa965a2f9bac560783
Steven L Lerner <stevenllerner@gmail.com>
2017-08-11 23:54:08 -0500

    functionally working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //Define variables
-var guessRemain = 12; 	// Number of guesses remaining
+var guessRemain = 12; 	// Number of guesses allowed
 var i = 1;				// Index
 var wordBank = [		// List of words to be guesses
 	'Guggenheim Museum',
@@ -18,8 +18,8 @@ var display;			// Displays dashes and letters to show current game status
 
 var guessLetter = "";		// Letter picked by player
 var guessedLetters = "";	// Keeps track of all guessed letters
-var numOfMatches;
-var numOfWins = 0;
+var numOfMatches;			// Number of times selected letter matches letters in word
+var numOfWins = 0;			// Number of games won by player
 
 //FUNCTIONS
 //Sets up initial sequence of dashes
@@ -42,29 +42,38 @@ function updateDisplay (guessLetter, word, display){
 	numOfMatches = 0;
 	for (k=0; k < word.length; k++){
 		if (word[k] === guessLetter) {
-			display[k] = guessLetter;
+			console.log(display);
+			display = display.substring(0,k) + guessLetter + display.substring(k+1,word.length);
+			console.log(display);
 			numOfMatches = numOfMatches + 1;
-		} 
+			console.log(guessLetter);
+			console.log(display[k]);
+			x = display;
+		} else {
+			x = display;
+		}
 	}
 	document.getElementById("wordStatus").innerHTML = display;
-	return display, numOfMatches;
+	return x, numOfMatches;
 }
 
 //Function updates guesses remaining
 function guessesRemaining (guessRemain, numOfMatches) {
 	if (numOfMatches === 0) {
 		guessRemain = guessRemain -1;
+		y = guessRemain
 	} else {
-		guessRemain = guessRemain;
+		y= guessRemain
 	}
-	document.getElementById("guessesRemaining").innerHTML = guessRemain;
-	return guessRemain;
+	document.getElementById("guessesRemaining").innerHTML = y;
+	return y;
 }
 
 //Function undates list of guessed letters
 function allGuessedLetters (guessedLetters, guessLetter) {
-	guessedLetters = guessedLetters + guessLetter;
-	return guessedLetters;
+	z = guessedLetters + guessLetter;
+	document.getElementById("guessedLetters").innerHTML = z;
+	return z;
 }
 
 document.onkeyup = function (event) {
@@ -74,49 +83,23 @@ document.onkeyup = function (event) {
 	document.getElementById("totNumOfWins").innerHTML = numOfWins;
 	document.getElementById("currentWord").innerHTML = "Current Word";
 	display = initialDisplay(word);
-	document.getElementById("wordStatus").innerHTML = display;
 	document.getElementById("numOfGuessesRemaining").innerHTML = "Number of Guesses Remaining";
 	document.getElementById("guessesRemaining").innerHTML = guessRemain;	
 	document.getElementById("lettersAlreadyGuessed").innerHTML = "Letters Already Guessed";
 	document.onkeyup = function (event) {
 		guessLetter = event.key.toUpperCase();
+		updateDisplay (guessLetter, word, display);
+		display = x;
+		guessesRemaining (guessRemain, numOfMatches);
+		guessRemain = y;
+		allGuessedLetters (guessedLetters, guessLetter);
+		guessedLetters = z;
+
+		if (y ,== 0) {
+			document.getElementById("outcome").innerHTML = "Sorry, you lost this game. Try again!";
+		}
+		if (display.includes("_") !== true) {
+			document.getElementById("outcome").innerHTML = "Congratulations! You've won this game!";
+		}
 	}
-
-
 }
-/*	document.onkeyup = function (event) {
-		document.getElementById("welcome").innerHTML = "Welcome to the game";
-		document.getElementById("instructions").innerHTML = "Please select a letter";
-			if (guessRemain > 0) {
-				
-					guessedLetters = guessedLetters + guessLetter;
-					document.getElementById("guessedLetters").innerHTML = guessedLetters;
-					//Compare selected letter to letters in word
-					numOfMatches = 0;
-					for (k=0; k < word.length; k++){
-
-						if (word[k] === guessLetter) {
-							display[k] = guessLetter;
-							numOfMatches = numOfMatches + 1;
-							console.log(display);
-						} 
-					}
-					//Update display
-					console.log(display);
-					document.getElementById("wordStatus").innerHTML = display;
-					//Update remaining guesses
-					if (numOfMatches < 1) {
-						guessRemain = guessRemain -1;
-					}
-					document.getElementById("guessesRemaining").innerHTML = guessRemain;		
-				}		
-			}
-
-*/
-
-
-
-
-
-
-
